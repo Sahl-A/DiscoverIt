551a0a35cffeddcf1734d07570a4d60fefd26511
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-12-23 23:50:50 +0200

    25: fix logout redirect

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -69,7 +69,8 @@ export const logoutUser = () => {
     localStorage.removeItem("DiscoverItToken");
     delete axios.defaults.headers.common["Authorization"];
     dispatch({ type: SET_UNAUTHENTICATED });
-    window.location.href = "/login"
+    const host = window.location.origin
+    window.location.href = `${host}/login`
   };
 };
 
